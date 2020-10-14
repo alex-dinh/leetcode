9994c9ef3381d78657714735bf72c04a33cc5395
unknown <alexdinh1028@gmail.com>
2020-10-14 15:30:29 -0700

    updates

--- OA/MaxProfitFromSuppliers.py
+++ OA/MaxProfitFromSuppliers.py
@@ -7,71 +7,28 @@ optimal solution:
 https://leetcode.com/discuss/interview-question/823177/amazon-oa-2020-find-the-highest-profit/713075
 '''
 
-import heapq
+'''
+Amazon Basics has several suppliers for its products.
+The inventory available from each supplier is given a list of numbers.
+Let's assume Amazon's profit on any single item = quantity remaining
+
+If a supplier has 4 items, the profit on the first item is 4, then 3, then 2, then 1.
+https://leetcode.com/discuss/interview-question/868302/
+
+Find the maximum amount of profit given the number of suppliers, the inventory for each supplier, 
+and the customer's order (in number of units order)
+'''
+
+
 from typing import List
 
-# naive approach: use heap (python's heapq), O (n^2) solution
-def highest_profit_heap(numSuppliers: int, inventory: List[int], order: int):
-    '''
-    :param numSuppliers: number of suppliers
-    :param inventory: list of ints, represent value of item at given supplier
-    :param order: number of items that the customer has ordered
-    :return: highest amount of profit that can be generated for given item
-
-    note: if supplier has n items, the amount of profit is n! (n factorial)
-                          4 items,                         4 + 3 + 2 + 1
-    in other words: profit on item = # of items supplier has left
-    note 2: this problem in Java specifies long ints, but doesn't matter in python
-    '''
-
-    profit = 0
-    inv = [-i for i in inventory]
-    heapq.heapify(inv)  # O(n)
-    for i in range(order): # O(n)
-        item = heapq.heappop(inv) # O(log n)
-        profit += -item
-        heapq.heappush(inv, item + 1) # O(log n)
-        # print(item)
-
-    return profit
-
-# optimal approach: O(n), using hashmap/dict
-def highest_profit(numSuppliers: int, inventory: List[int], order: int):
-    '''
-    :param numSuppliers: number of suppliers
-    :param inventory: list of ints, represent value of item at given supplier
-    :param order: number of items that the customer has ordered
-    :return: highest amount of profit that can be generated for given item
-
-    note: if supplier has n items, the amount of profit is n! (n factorial)
-                          4 items,                         4 + 3 + 2 + 1
-    in other words: profit on item = # of items supplier has left
-    note 2: this problem in Java specifies long ints, but doesn't matter in python
-    '''
-
-    profits = {}
-    for item in inventory: # O(n)
-        profits[item] = profits.get(item, 0) + 1
-
-    mpi = max(inventory) # most profitable item, O(n)
-    max_profit = 0
-    for i in range(order): # O(n)
-        max_profit += mpi
-        profits[mpi - 1] = profits.get(mpi - 1, 0) + 1
-        if profits[mpi] == 1:
-            del profits[mpi]
-            mpi -= 1
-        elif profits[mpi] > 1:
-            profits[mpi] -= 1
-
-    return max_profit
-
-def highest_profit_lc(numSuppliers: int, inventory: List[int], order: int):
+# optimized everything
+def highest_profit_comments(numSuppliers: int, inventory: List[int], order: int):
     # key principle: given the current max price, the next largest max prices is always 1 less
 
     # map, item price : number of items we can sell at the price
     profit = dict()
-    for price in inventory:
+    for price in inventory: # price = quantity
         profit[price] = profit.get(price, 0) + 1
 
     # the current most profitable item price,
@@ -106,23 +63,41 @@ def highest_profit_lc(numSuppliers: int, inventory: List[int], order: int):
 
     return res
 
+def highest_profit(numSuppliers: int, inventory: List[int], order: int):
+    profit = dict() # dictionary to keep track of prices
+    for price in inventory:
+        profit[price] = profit.get(price, 0) + 1
+
+    curr_max = max(profit.items(), key=lambda x: x[0])[0]
+
+    res = 0
+    while order > 0:
+        maxi = min(order, profit[curr_max])
+        res += curr_max * maxi
+        order -= maxi
+        profit[curr_max] -= maxi
+        profit[curr_max - 1] = profit.get(curr_max - 1, 0) + maxi
+        if profit[curr_max] == 0:
+            del profit[curr_max]
+            curr_max -= 1
+
+    '''
+    N: size of inventory
+    K: number of items in the order
+    time complexity: O(N + K) = O(N) or O(K)
+    # Space: O(N)
+    '''
+    return res
+
 numSuppliers = 2
 inventory = [3, 5]
 order = 6
 # max profit = 5 + 4 + 3 + 3 + 2 + 2 = 19
 print(highest_profit(numSuppliers, inventory, order))
-print(highest_profit_heap(numSuppliers, inventory, order))
 
-numSuppliers = 2
-inventory = [5, 5]
-order = 4
-# max profit = 5 + 5 + 4 + 4 = 18
-print(highest_profit(numSuppliers, inventory, order))
-print(highest_profit_heap(numSuppliers, inventory, order))
 
-numSuppliers = 1
-inventory = [5]
-order = 5
-# max profit = 5 + 4 + 3 + 2  + 1 = 15
-print(highest_profit(numSuppliers, inventory, order))
-print(highest_profit_heap(numSuppliers, inventory, order))
+
+
+
+
+
--- OA/MaximumUnits.py
+++ OA/MaximumUnits.py
@@ -14,25 +14,29 @@ def getMaxUnits(boxes: List[int], unitsPerBox: List[int], truckSize: int) -> int
     '''
 
     # build a maxheap
+    # IMPORTANT NOTE: heapify() is O(n), because it takes a different amount of time for each node
     heap = []
-    for i in range(len(boxes)):
+    for i in range(len(boxes)): # O(n)
         units_per_box = unitsPerBox[i]
         # by default python heapq is a minheap, so negate values to sim a maxheap,
         # most negative nums at top of heap
+        #   heapq is sorted by first item in pair, the num of units
         heapq.heappush(heap, (-units_per_box, boxes[i]))
 
     res = 0
 
     # "adding" boxes to the truck
     # get the box with most units from heap, calculate how many units can be shipped
-    # taking min() is faster than iterating 1 by 1
-    while truckSize > 0 and heap:
-        curr_max = heapq.heappop(heap)
-        max_boxes = min(truckSize, curr_max[1])
+    # taking min() is faster than iterating 1 by 1, reduces number of iterations in loop
+    while truckSize > 0 and heap: # O(t), t = truck size
+        curr_max = heapq.heappop(heap) # O(log n), n is
+        max_boxes = min(truckSize, curr_max[1]) # while min() is typically O(n), n always == 2, so O(1) in this context
         truckSize -= max_boxes
         res += max_boxes * (curr_max[0] * -1)
 
+    # FINAL TIME COMPLEXITY: O(T LOG N), T is truckSize, N is size of boxes[]
     return res
+
 
 
 boxes = [1, 2, 3]
